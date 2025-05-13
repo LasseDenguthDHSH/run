2691a5caf7e9aea2834542b0e8e5b8a5dbababae
lasse <lasse.denguth@std.dhsh.de>
2025-05-13 11:37:01 +0200

    sa

--- src/Level.java
+++ src/Level.java
@@ -1,7 +1,15 @@
 package src;
-
 public abstract class Level {
+    protected int groundY;
+    protected String backgroundImage;
 
-    int level;
+    public abstract void setupLevel();
 
+    public int getGroundY() {
+        return groundY;
+    }
+
+    public String getBackgroundImage() {
+        return backgroundImage;
+    }
 }
--- src/Level1.java
+++ src/Level1.java
@@ -1,4 +1,11 @@
 package src;
 
 public class Level1 extends Level {
+    @Override
+    public void setupLevel() {
+        groundY = 300;
+        backgroundImage = "assets/images/level1_bg.png";
+    }
 }
+
+
