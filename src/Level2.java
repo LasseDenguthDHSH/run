df03f9b20c5ff667e7ad364ee256e4ed2a114977
Leif Schönauer <mail@leifschoenauer.de>
2025-05-15 10:20:58 +0200

    level 2 - updated

--- src/Level2.java
+++ src/Level2.java
@@ -6,13 +6,14 @@ import java.awt.*;
 public class Level2 extends Level {
     public Level2(String title) {
         super(title);
-        this.groundY = 700;
+        this.groundY = 800;
         this.groundImage = loadImage("src/images/groundLevel2.png");
-        this.skyImage = loadImage("src/images/Level2.png");
+        this.skyImage = loadImage("src/images/skyLevel2.png");
         this.player1Image = loadImage("src/images/player1.png");
         this.player2Image = loadImage("src/images/player2.png");
         this.platformColor = Color.gray;
         // Plattformen hinzufügen
+        platforms.add(new Platform(0, 700, 150, 20));
         platforms.add(new Platform(200, 600, 150, 20));
         platforms.add(new Platform(450, 500, 150, 20));
     }
